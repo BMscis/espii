c838e0e70ba08370e341e3f6bc2a77b9043433b2
BMscis <melvinwafula@gmail.com>
2019-11-20 15:50:23 +0300

    Add files via upload

---
+++ app-signup/gin.js
@@ -0,0 +1,14 @@
+import {MDCTextField} from '@material/textfield';
+
+const username = new MDCTextField(document.querySelector('.username'));
+const password = new MDCTextField(document.querySelector('.password'));
+
+const businessname = new MDCTextField(document.querySelector('.businessname'));
+const email = new MDCTextField(document.querySelector('.email'));
+
+import {MDCRipple} from '@material/ripple';
+
+const cancel = new MDCRipple(document.querySelector('.cancel'));
+const next = new MDCRipple(document.querySelector('.next'));
+
+

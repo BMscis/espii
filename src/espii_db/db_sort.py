b7defd69eff984eb1f2720448ca330c79ca09598
BMscis <melvinwafula@gmail.com>
2020-02-25 02:41:17 +0300

    db

--- src/espii_db/db_sort.py
+++ src/espii_db/db_sort.py
@@ -6,45 +6,55 @@ from datetime import datetime
 from datetime import timedelta
 from mysql.connector import errorcode
 
-data = open('log.json')
+class Espii:
+
+    def __init__(self, json_data, user,password,database):
+        self.json_data = json_data
+        self.user = user
+        self.password = password
+        self.database = database
+        self.cnx = mysql.connector.connect(user=self.user, password=self.password,host='127.0.0.1',database=self.database)
+        self.cursor = self.cnx.cursor()
+
+    def execute_mysql (self,table_name, columns,values,cursor):
+
+        add_data="INSERT INTO {}({}) VALUES{}".format(table_name,columns,values)
+
+        cursor.execute(add_data)
+        print(add_data)
+
+    def get_data (self):
+        for i in range(0, len(self.json_data)):
+            try:
+                if len(self.json_data[i]['metadata']['music'])> 1:
+                    
+                    for x in range(0, len(self.json_data[i]['metadata']['music'])):
+                        get_timestamp = self.json_data[i]['metadata']['timestamp_utc']
+                        timestamp_date = datetime.strptime(str(get_timestamp), '%Y-%m-%d %H:%M:%S')+timedelta(hours=3)
+                        timestamp_change = timestamp_date + timedelta(seconds=x)
+                        timestamp = datetime.strftime(timestamp_change, '%Y-%m-%d %H:%M:%S')
+                        get_played_duration = self.json_data[i]['metadata']['played_duration']*1000
+                        played_duration = get_played_duration + x
+                        acrid = self.json_data[i]['metadata']['music'][x]['acrid']
+                        score= self.json_data[i]['metadata']['music'][x]['score']
+                        self.execute_mysql('time_stamp','timestamp, played_duration, acrid, score',(str(timestamp_change),str(played_duration),str(acrid),str(score)),self.cursor)
+                else:
+                    get_timestamp = self.json_data[i]['metadata']['timestamp_utc']
+                    timestamp = datetime.strptime(str(get_timestamp), '%Y-%m-%d %H:%M:%S')+timedelta(hours=3)
+                    played_duration = self.json_data[i]['metadata']['played_duration']*1000
+                    acrid = self.json_data[i]['metadata']['music'][0]['acrid']
+                    score= self.json_data[i]['metadata']['music'][0]['score']
+                    self.execute_mysql('time_stamp','timestamp, played_duration, acrid, score',(str(timestamp),str(played_duration),str(acrid),str(score)),self.cursor)
+            except mysql.connector.IntegrityError:
+                continue
+
+    def commit(self):
+        self.cnx.commit()
+        self.cursor.close()
+        self.cnx.close()
+
+
+data = open('C:/Users/melvi/log8.json')
 results = json.load(data)
-
-cnx= mysql.connector.connect(user='root', password='Meddickmeddick6',host='127.0.0.1',database="monitor_results")
-cursor = cnx.cursor()
-
-def call_option (table_name, columns=str(),values=()):
-
-    add_data="INSERT INTO {}({}) VALUES{}".format(table_name,columns,values)
-
-    cursor.execute(add_data)
-    print(add_data)
-
-
-for i in range(0, len(results)):
-    try:
-        if len(results[i]['metadata']['music'])> 1:
-            
-            for x in range(0, len(results[i]['metadata']['music'])):
-                get_timestamp = results[i]['metadata']['timestamp_utc']
-                timestamp_date = datetime.strptime(str(get_timestamp), '%Y-%m-%d %H:%M:%S')+timedelta(hours=3)
-                timestamp_change = timestamp_date + timedelta(seconds=x)
-                timestamp = datetime.strftime(timestamp_change, '%Y-%m-%d %H:%M:%S')
-                get_played_duration = results[i]['metadata']['played_duration']*1000
-                played_duration = get_played_duration + x
-                acrid = results[i]['metadata']['music'][x]['acrid']
-                score= results[i]['metadata']['music'][x]['score']
-                call_option('time_stamp','timestamp, played_duration, acrid, score',(str(timestamp_change),str(played_duration),str(acrid),str(score)))
-        else:
-            get_timestamp = results[i]['metadata']['timestamp_utc']
-            timestamp = datetime.strptime(str(get_timestamp), '%Y-%m-%d %H:%M:%S')+timedelta(hours=3)
-            played_duration = results[i]['metadata']['played_duration']*1000
-            acrid = results[i]['metadata']['music'][0]['acrid']
-            score= results[i]['metadata']['music'][0]['score']
-            call_option('time_stamp','timestamp, played_duration, acrid, score',(str(timestamp),str(played_duration),str(acrid),str(score)))
-    except mysql.connector.IntegrityError:
-        continue
-
-cnx.commit()
-cursor.close()
-cnx.close()    
-
+handler = Espii(results,'root','Meddickmeddick6','monitor_results')
+handler.get_data()

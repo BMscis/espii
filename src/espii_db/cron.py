2ca782861e321861e9b5e80ecc2bc3ae1e6a8c8e
BMscis <melvinwafula@gmail.com>
2020-03-15 23:59:17 +0300

    cron

--- src/espii_db/cron.py
+++ src/espii_db/cron.py
@@ -2,7 +2,7 @@ from crontab import CronTab
 
 espii_cron = CronTab(user='root')
 job = espii_cron.new(command='python2 /var/www/html/espii/src/espii_db/acr_callback_api.py',comment='callback')
-job2 = espii_cron.new(command='python2 /var/www/html/espii/src/espii_db/db_sort.py',comment='call_database')
+job2 = espii_cron.new(command='python3 /var/www/html/espii/src/espii_db/db_sort.py',comment='call_database')
 job.minute.every(5)
 job2.minute.every(6)
 espii_cron.write()
